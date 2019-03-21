1755d7e9d9e5c9c638b37fbc4fab4a958a8f39b9
Eesh Chilukalapalli <eeshlikhith@gmail.com>
2019-03-21 22:26:15 +0530

    Generate custom Scratch extension file

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ app.use(cors());
 
 app.use(express.static('public'))
 
+var exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 const fs = require('fs');
+
+app.get("/extension", (req, res) => {
+    res.contentType('application/javascript');
+    res.charset = 'UTF-8';
+    res.render('extension', {
+      apikey: 'oudhfiosdf'
+    });
+});
 
 
 app.listen(2323, () => {
